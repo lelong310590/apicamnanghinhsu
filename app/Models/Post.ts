1eec451345d68981206791de533ed5edee3bc88f
Long Le Ngoc <longln@tohsoft.com>
2023-04-27 23:28:54 +0700

    fix

--- app/Models/Post.ts
+++ app/Models/Post.ts
@@ -1,5 +1,5 @@
-import { DateTime } from 'luxon'
-import { BaseModel, BelongsTo, belongsTo, column, HasOne, hasOne, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import {DateTime} from 'luxon'
+import {BaseModel, BelongsTo, belongsTo, column, HasOne, hasOne, ManyToMany, manyToMany} from '@ioc:Adonis/Lucid/Orm'
 import User from './User'
 import Type from './Type'
 import Category from './Category'
@@ -7,72 +7,78 @@ import Tag from './Tag'
 import Slug from './Slug'
 
 export default class Post extends BaseModel {
-  @column({ isPrimary: true })
-  public id: number
+    @column({isPrimary: true})
+    public id: number
 
-  @column()
-  public name: string
+    @column()
+    public name: string
 
-  @column()
-  public description: string | null
+    @column()
+    public description: string | null
 
-  @column()
-  public content: string | null
+    @column()
+    public content: string | null
 
-  @column()
-  public status: string
+    @column()
+    public status: string
 
-  @column()
-  public authorId: number
+    @column()
+    public authorId: number
 
-  @column()
-  public isFeatured: number
+    @column()
+    public isFeatured: number
 
-  @column()
-  public image: string | null
+    @column()
+    public image: string | null
 
-  @column()
-  public views: number
+    @column()
+    public views: number
 
-  @column()
-  public formatType: string | null
+    @column()
+    public formatType: string | null
 
-  @column()
-  public typeId: number
+    @column()
+    public typeId: number
 
-  @column()
-  public nextId: number | undefined | null
+    @column()
+    public nextId: number | undefined | null
 
-  @column()
-  public prevId: number | undefined | null
+    @column()
+    public prevId: number | undefined | null
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+    @column.dateTime({autoCreate: true})
+    public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+    @column.dateTime({autoCreate: true, autoUpdate: true})
+    public updatedAt: DateTime
 
-  //declare relationship
-  @belongsTo(() => User,{
-    foreignKey: 'authorId'
-  })
-  public author: BelongsTo<typeof User>
+    @column.dateTime()
+    public releaseDate: DateTime
 
-  @belongsTo(() => Type)
-  public type: BelongsTo<typeof Type>
+    @column.dateTime()
+    public effectDate: DateTime
 
-  @manyToMany(() => Category,{
-    pivotTable: 'post_categories'
-  })
-  public category: ManyToMany<typeof Category>
+    //declare relationship
+    @belongsTo(() => User, {
+        foreignKey: 'authorId'
+    })
+    public author: BelongsTo<typeof User>
 
-  @manyToMany(() => Tag,{
-    pivotTable: 'post_tags'
-  })
-  public tag: ManyToMany<typeof Tag>
+    @belongsTo(() => Type)
+    public type: BelongsTo<typeof Type>
 
-  @hasOne(() => Slug, {
-    foreignKey:'referenceId'
-  })
-  public slug: HasOne<typeof Slug>
+    @manyToMany(() => Category, {
+        pivotTable: 'post_categories'
+    })
+    public category: ManyToMany<typeof Category>
+
+    @manyToMany(() => Tag, {
+        pivotTable: 'post_tags'
+    })
+    public tag: ManyToMany<typeof Tag>
+
+    @hasOne(() => Slug, {
+        foreignKey: 'referenceId'
+    })
+    public slug: HasOne<typeof Slug>
 }
